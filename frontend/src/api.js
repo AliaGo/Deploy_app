964a10f953925fb77da0716d319c1bed535c0a47
AliaGo <75656727+AliaGo@users.noreply.github.com>
2022-12-29 19:12:09 +0800

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,12 +3,12 @@ import axios from 'axios';
 const API_ROOT = 
   process.env.NODE_ENV === "production"
     ? "/api"
-    : "http://localhost:4001/";
+    : "http://localhost:4000/";
 /*
 const WS_URL =
   process.env.NODE_ENV === "production"
     ? window.location.origin.replace(/^http/, "ws")
-    : "ws://localhost:4001";
+    : "ws://localhost:4000";
 */
 export const api = axios.create({baseURL: API_ROOT});
 //export const ws = new WebSocket(WS_URL);
