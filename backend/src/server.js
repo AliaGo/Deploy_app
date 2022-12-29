824a27fbb607a8648464bc9ac37e53c23435e675
AliaGo <candy30307@gmail.com>
2022-12-29 23:29:54 +0800

    v6

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,66 +1,33 @@
-import express from 'express';
-import bodyParser from 'body-parser';
 import cors from 'cors';
+import dotenv from "dotenv-defaults";
+import mongoose from 'mongoose';
+import path from "path";
+
+import express from 'express';
 import db from './db';
 import routes from './routes';
-import path from 'path';
-import ScoreCard from './models/ScoreCard';
-
-db.connect();
-
 
 const app = express();
 
-
-
-if(process.env.NODE_ENV === "development"){
-    app.use(cors());
-}
-
-//app.use(cors());
-app.use(express.json());
-app.use(bodyParser.json());
-app.use('/', routes);
-
-
-if(process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === "production") {
+    console.log("production mode");
     const __dirname = path.resolve();
     app.use(express.static(path.join(__dirname, "../frontend", "build")));
-    app.get("/*", function(req, res) {
+    app.get("/*", function (req, res) {
         res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
     });
 }
-const port = process.env.PORT || 4001;
-app.get('/', (req, res) => {
+else {
+    console.log("development mode")
+}
 
- res.send('Hello, World!');
-});
+app.use(cors());
+app.use(express.json());
+app.use('/api', routes);
+db.connect();
+
+// define server
+const port = process.env.PORT || 4000;
 app.listen(port, () =>
- console.log(`Example app listening on port ${port}!`),
+    console.log(`Server is up on port ${port}.`)
 );
-
-db.on('error', (err) => console.error('connection error', err)); // 連線異常
-db.once('open', (db) => console.log('Connected to MongoDB')); // 連線成功
-/*
-const saveScore = async (name, subject, score) => {
-    const existing = await ScoreCard.findOne({ name, subject });
-    if (existing) throw new Error(`data ${name, subject} exists!!`);
-    try {
-        const newScore = new ScoreCard({ name, subject, score });
-        console.log("Created score", newScore);
-        return newScore.save();
-    } catch (e) { throw new Error("Score creation error: " + e); }
-   };
-const deleteDB = async () => {
-    try {
-        await ScoreCard.deleteMany({});
-        console.log("Database deleted");
-    } catch (e) { throw new Error("Database deletion failed"); }
-};
-db.on("error", (err) => console.log(err));
-db.once("open", async () => {
- await deleteDB();
- await saveScore("Alia", "math", 100);
- await saveScore("Sandy", "geography", 75);
- await saveScore("Peter", "economics", 70);
-});*/
